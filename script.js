85d511c6a82be5fda9e3ae17d1d064293d6f7ce9
Priyanshu Patel <139453653+code-withpriyanshu@users.noreply.github.com>
2024-06-13 22:05:41 +0530

    mini circle added

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+function movecircle() {
+  let circle = document.getElementById("minicircle");
+  window.addEventListener("mousemove", function (del) {
+    let x = del.clientX;
+    let y = del.clientY;
+    circle.style.transform = `translate(${x}px , ${y}px)`;
+  });
+}
+movecircle();
